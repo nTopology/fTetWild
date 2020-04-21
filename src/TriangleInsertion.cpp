02941dfaf0a716631f95bed6858725d6522184e2
Suraj Musuvathy <surajmusuvathy@ntopology.com>
2020-04-21 17:50:55 -0400

    use default random generator seed

--- src/TriangleInsertion.cpp
+++ src/TriangleInsertion.cpp
@@ -140,7 +140,7 @@ void floatTetWild::sort_input_faces(const std::vector<Vector3> &input_vertices, 
     if (mesh.params.not_sort_input)
         return;
 
-    std::shuffle(sorted_f_ids.begin(), sorted_f_ids.end(), std::mt19937(0));
+    std::shuffle(sorted_f_ids.begin(), sorted_f_ids.end(), std::mt19937());
 
 //    std::sort(sorted_f_ids.begin(), sorted_f_ids.end(), [&weights](int a, int b) {
 //        return weights[a] < weights[b];
